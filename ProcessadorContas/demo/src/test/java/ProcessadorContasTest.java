22dbc432ecc20c537b74c4981a3e187ff117ba86
fabiomaciel7 <fabio.maciel@ccc.ufcg.edu.br>
2024-07-31 20:48:56 -0300

    [TEST] testProcessarContasFaturaPendente

--- ProcessadorContas/demo/src/test/java/ProcessadorContasTest.java
+++ ProcessadorContas/demo/src/test/java/ProcessadorContasTest.java
@@ -42,7 +42,11 @@ public class ProcessadorContasTest {
 
     @Test
     void testProcessarContasFaturaPendente(){
+        fatura.adicionarConta(new Conta(001, LocalDate.of(2023, Month.FEBRUARY, 6), 700, "CARTAO_CREDITO"));
+        fatura.adicionarConta(new Conta(002, LocalDate.of(2023, Month.FEBRUARY, 17), 800, "TRANSFERENCIA_BANCARIA"));
 
+        processador.processar(fatura);
+        assertEquals(fatura.getStatus(),"PENDENTE");
     }
 
 }
