20ccc0d9aaab9997def8775af904a27901dbc83a
fabiomaciel7 <fabio.maciel@ccc.ufcg.edu.br>
2024-07-31 21:32:04 -0300

    [REFACTOR] Classe ProcessadorContas melhorada

--- ProcessadorContas/demo/src/main/java/com/example/processador/ProcessadorContas.java
+++ ProcessadorContas/demo/src/main/java/com/example/processador/ProcessadorContas.java
@@ -1,51 +1,64 @@
 package com.example.processador;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.time.LocalDate;
 
 public class ProcessadorContas {
 
-
-    public ProcessadorContas(){
-
+    public ProcessadorContas() {
     }
 
-    public void processar(Fatura fatura){
+    public void processar(Fatura fatura) {
         double somaPagamentos = 0;
 
-        List<Conta> contas = new ArrayList<>();
-        contas = fatura.getContas();
-        for (Conta conta: contas){
-            Pagamento pagamento = new Pagamento(conta.getValorPago(),conta.getData(),conta.getTipoPagamento());
+        List<Conta> contas = fatura.getContas();
+        for (Conta conta : contas) {
+            Pagamento pagamento = new Pagamento(conta.getValorPago(), conta.getData(), conta.getTipoPagamento());
 
-            if(pagamento.getTipoPagamento().equals("BOLETO")){
-                if (pagamento.getValorPago() >= 0.01 && pagamento.getValorPago() <= 5000.00) {
-                    if (pagamento.getData().isAfter(fatura.getData())) {
-                        pagamento.setValorPago(pagamento.getValorPago() * 1.10);
-                        somaPagamentos += pagamento.getValorPago();
-                    }
-                    else{
-                        somaPagamentos += pagamento.getValorPago();
-                    }
-                }
-            } else if (pagamento.getTipoPagamento().equals("CARTAO_CREDITO")) {
-                if (conta.getData().isBefore(fatura.getData().minusDays(15)) || conta.getData().isEqual(fatura.getData().minusDays(15))) {
-                    somaPagamentos += pagamento.getValorPago();
-                }
-            } else if (pagamento.getTipoPagamento().equals("TRANSFERENCIA_BANCARIA")) {
-                if(!conta.getData().isAfter(fatura.getData())){
-                    somaPagamentos += pagamento.getValorPago();
-                }
+            switch (pagamento.getTipoPagamento()) {
+                case "BOLETO":
+                    somaPagamentos += processarBoleto(pagamento, fatura.getData());
+                    break;
+                case "CARTAO_CREDITO":
+                    somaPagamentos += processarCartaoCredito(pagamento, fatura.getData());
+                    break;
+                case "TRANSFERENCIA_BANCARIA":
+                    somaPagamentos += processarTransferenciaBancaria(pagamento, fatura.getData());
+                    break;
+                default:
+                    break;
             }
         }
 
-        if(somaPagamentos >= fatura.getValorTotal()){
+        if (somaPagamentos >= fatura.getValorTotal()) {
             fatura.setStatus("PAGA");
-        }
-        else{
+        } else {
             fatura.setStatus("PENDENTE");
         }
-        
     }
-    
+
+    private double processarBoleto(Pagamento pagamento, LocalDate dataFatura) {
+        double valorPago = pagamento.getValorPago();
+        if (valorPago >= 0.01 && valorPago <= 5000.00) {
+            if (pagamento.getData().isAfter(dataFatura)) {
+                valorPago *= 1.10;
+            }
+            return valorPago;
+        }
+        return 0;
+    }
+
+    private double processarCartaoCredito(Pagamento pagamento, LocalDate dataFatura) {
+        if (pagamento.getData().isBefore(dataFatura.minusDays(15)) || pagamento.getData().isEqual(dataFatura.minusDays(15))) {
+            return pagamento.getValorPago();
+        }
+        return 0;
+    }
+
+    private double processarTransferenciaBancaria(Pagamento pagamento, LocalDate dataFatura) {
+        if (!pagamento.getData().isAfter(dataFatura)) {
+            return pagamento.getValorPago();
+        }
+        return 0;
+    }
 }
