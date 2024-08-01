ec176b534ea332fa157dae6a923ff5c858109af2
Miguel Rodrigues <miguel.rodrigues@ccc.ufcg.edu.br>
2024-07-31 21:32:31 -0300

    Criacao testes para Show

---
+++ SistemaIngressos/tests/tests/ShowTest.java
@@ -0,0 +1,32 @@
+import static org.junit.Assert.assertNotNull;
+import org.junit.Before;
+import org.junit.Test;
+import java.util.Date;
+
+import sistema.Ingresso;
+import sistema.TipoIngresso;
+import sistema.LoteDeIngressos;
+import sistema.Show;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.jupiter.api.BeforeEach;
+
+public class ShowTest {
+    private Show show;
+
+    @Before
+    public void setUp() {
+        show = new Show(new Date(), "Artista Famoso", 1000.0, 2000.0, true);
+    }
+
+    @Test
+    public void testCriacaoShow() {
+        assertNotNull(show);
+        assertNotNull(show.getData());
+        assertNotNull(show.getArtista());
+    }
+}
