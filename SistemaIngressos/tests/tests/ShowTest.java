dca882e4acf8346014a35c174e586783188ea81e
Miguel Rodrigues <miguel.rodrigues@ccc.ufcg.edu.br>
2024-07-31 21:53:31 -0300

    Adicionando mais testes para show

--- SistemaIngressos/tests/tests/ShowTest.java
+++ SistemaIngressos/tests/tests/ShowTest.java
@@ -16,14 +16,31 @@ import static org.junit.jupiter.api.Assertions.*;
 import java.util.Arrays;
 import java.util.List;
 
+
+
 import org.junit.jupiter.api.BeforeEach;
 
 public class ShowTest {
-    private Show show;
+	private Show show;
+    private Ingresso ingressoVIP;
+    private Ingresso ingressoNormal;
+    private Ingresso ingressoMeiaEntrada;
+    private LoteDeIngressos lote;
 
     @Before
     public void setUp() {
-        show = new Show("31/07/2024", "Artista Famoso", 1000.0, 2000.0, true);
+    	ingressoVIP = new Ingresso(1, TipoIngresso.VIP);
+        ingressoNormal = new Ingresso(2, TipoIngresso.NORMAL);
+        ingressoMeiaEntrada = new Ingresso(3, TipoIngresso.MEIA_ENTRADA);
+
+        lote = new LoteDeIngressos(1, Arrays.asList(ingressoVIP, ingressoNormal, ingressoMeiaEntrada), 15.0);
+        show = new Show("Amanhã", "Artista Famoso", 1000.0, 2000.0, true);
+        show.adicionarLote(lote);
+
+        // Marcar todos os ingressos como vendidos
+        ingressoVIP.marcarComoVendido();
+        ingressoNormal.marcarComoVendido();
+        ingressoMeiaEntrada.marcarComoVendido();
     }
 
     @Test
@@ -33,5 +50,27 @@ public class ShowTest {
         assertEquals("31/07/2024", show.getData());
         assertNotNull(show.getArtista());
         assertEquals("Artista Famoso", show.getArtista());
+    }
+    
+    @Test
+    public void testGetTotalIngressosVendidos() {
+        assertEquals(1, show.getTotalIngressosVendidos(TipoIngresso.VIP));
+        assertEquals(1, show.getTotalIngressosVendidos(TipoIngresso.NORMAL));
+        assertEquals(1, show.getTotalIngressosVendidos(TipoIngresso.MEIA_ENTRADA));
+    }
+
+    @Test
+    public void testCalcularReceita() {
+        assertEquals(305.0, show.calcularReceita(), 0.001);
+    }
+
+    @Test
+    public void testCalcularLucro() {
+        assertEquals(-3095.0, show.calcularLucro(), 0.001);
+    }
+
+    @Test
+    public void testGetStatusFinanceiro() {
+        assertEquals("PREJUÍZO", show.getStatusFinanceiro());
     }
 }
