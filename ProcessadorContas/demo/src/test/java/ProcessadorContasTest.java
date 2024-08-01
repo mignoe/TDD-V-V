4193c80f76b60077d114c4da1bb873ce1047b17a
fabiomaciel7 <fabio.maciel@ccc.ufcg.edu.br>
2024-07-31 21:14:49 -0300

    novos testes

--- ProcessadorContas/demo/src/test/java/ProcessadorContasTest.java
+++ ProcessadorContas/demo/src/test/java/ProcessadorContasTest.java
@@ -41,9 +41,29 @@ public class ProcessadorContasTest {
     }
 
     @Test
-    void testProcessarContasFaturaPendente(){
+    void testProcessarContasFaturaPendentePorDataCartaoCredito(){
         fatura.adicionarConta(new Conta(001, LocalDate.of(2023, Month.FEBRUARY, 6), 700, "CARTAO_CREDITO"));
         fatura.adicionarConta(new Conta(002, LocalDate.of(2023, Month.FEBRUARY, 17), 800, "TRANSFERENCIA_BANCARIA"));
+
+        processador.processar(fatura);
+        assertEquals(fatura.getStatus(),"PENDENTE");
+    }
+
+    @Test
+    void testProcessarContasFaturaPendentePorTransferenciaBancaria(){
+        fatura.adicionarConta(new Conta(001, LocalDate.of(2023, Month.FEBRUARY, 5), 700, "CARTAO_CREDITO"));
+        fatura.adicionarConta(new Conta(002, LocalDate.of(2023, Month.FEBRUARY, 21), 800, "TRANSFERENCIA_BANCARIA"));
+        
+        processador.processar(fatura);
+        assertEquals(fatura.getStatus(),"PENDENTE");
+    
+    }
+
+    @Test
+    void testProcessarContasFaturaPendentePorValor(){
+        fatura.adicionarConta(new Conta(001, LocalDate.of(2023, Month.FEBRUARY, 20), 400, "BOLETO"));
+        fatura.adicionarConta(new Conta(002, LocalDate.of(2023, Month.FEBRUARY, 20), 400, "BOLETO"));
+        fatura.adicionarConta(new Conta(003, LocalDate.of(2023, Month.FEBRUARY, 20), 600, "BOLETO"));
 
         processador.processar(fatura);
         assertEquals(fatura.getStatus(),"PENDENTE");
